ff344105d3e6af75f92e7e8fd99aa2a45e2d04f5
Atif-reyaz-khan <atifreyazkhan@gmail.com>
2021-04-18 14:36:19 +0530

    Updates

---
+++ precache-manifest.cfe4efd0202b5fcafe6fe44368e66e22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b5301b0bd79584ad2be61244a81371",
+    "url": "/foodOderApp/index.html"
+  },
+  {
+    "revision": "40cd4079102267fbc3d8",
+    "url": "/foodOderApp/static/css/main.670959da.chunk.css"
+  },
+  {
+    "revision": "b6bee4dcfa9338e0f03f",
+    "url": "/foodOderApp/static/js/2.17f9fd44.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/foodOderApp/static/js/2.17f9fd44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40cd4079102267fbc3d8",
+    "url": "/foodOderApp/static/js/main.59ccb320.chunk.js"
+  },
+  {
+    "revision": "ef03c413f100822574fe",
+    "url": "/foodOderApp/static/js/runtime-main.5ce04602.js"
+  }
+]);
